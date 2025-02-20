72d011f6f22dab70a1d640cef622f8059403637f
TakWolf <takwolf@foxmail.com>
2025-02-20 14:16:53 +0800

    添加 GitHub 测试工作流

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,15 @@
 import { HardhatUserConfig, vars } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
+const INFURA_API_KEY = vars.get('INFURA_API_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+const DEPLOY_PRIVATE_KEY = vars.get('POLYGON_DEPLOY_PRIVATE_KEY', '0000000000000000000000000000000000000000000000000000000000000000')
+
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
   networks: {
     polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${vars.get('INFURA_API_KEY')}`,
-      accounts: [
-        vars.get('POLYGON_DEPLOY_PRIVATE_KEY'),
-      ],
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [DEPLOY_PRIVATE_KEY],
     },
   },
 }
